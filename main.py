680c442c74e54b4a6992eb274502a9c919766a7d
phoenix121 <wsp5082@gmail.com>
2024-07-31 20:56:03 +0100

    version 1

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import flask
-from os import path, walk
+import os
+from pathlib import Path
 from flask_socketio import send, emit, SocketIO
+import threading
 import logging
+import json
+import time
+from math import floor
+
 
 import timer
 
 
-id = ["TwitchT1", "TwitchT2", "TwitchT3", "Bits"]
-timerTracker = timer.Timer(timeLeft=10)
+conversions = {
+    "TwitchT1": [int, 180],
+    "TwitchT2": [int, 180],
+    "TwitchT3": [int, 180],
+    "TwitchBits": [int, 180 / 200],
+    "StreamElements": [float, 180 / 2],
+    "YTSuperChat": [float, 180 / 1.46],  # in USD btw not won
+    "YTMembership": [int, 180],
+    "Afreeca": [int, 180 / 200],
+    "CHZZK": [int, 180 / 2000]
+}
+
+timerTracker = timer.Timer(0)
+
+history = {}
+if os.path.isfile("history.json"):
+    history = json.load(open("history.json", "r"))
+    timerTracker.endTimeReference = history["time"]
+else:
+    history = {"time": None, "points": 0, "log": []}
 
 app = flask.Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -55,16 +79,107 @@ def addSeconds(data):
     timerTracker.addSeconds(data["seconds"])
 
 
+@socketio.on("addTimerInfo")
+def addTimerInfo(data):
+    print("Adding Timer Info", data)
+
+    subType = data["type"]
+
+    if subType not in conversions:
+        emit("result", {
+            "type": "add-error",
+            "reason": f"SUBSCRIPTION TYPE NOT FOUND"
+        })
+        return
+
+    castFunction = conversions[subType][0]
+    quantityCast = 0
+    try:
+        quantityCast = castFunction(data["quantity"])
+    except ValueError:
+        emit("result", {
+            "type": "add-error",
+            "reason": f"{data['quantity']} should be a {castFunction.__name__}"
+        })
+        return
+
+    seconds = conversions[subType][1] * quantityCast
+    history["log"].insert(0, {
+        "#": len(history["log"]) + 1,
+        "type": subType,
+        "quantity": quantityCast,
+        "seconds": seconds,
+        "points": seconds / 180,
+    })
+    timerTracker.addSeconds(seconds)
+
+    history["time"] = timerTracker.endTimeReference
+    history["points"] += seconds / 180
+
+    outFile = open("history.json", "w+")
+    json.dump(history, outFile)
+    outFile.close()
+
+    emit("result", {
+        "type": "add-info",
+    })
+
+    emit("result", {
+        "type": "history-update",
+        "history": history["log"][:5]
+    })
+
+
+@socketio.on("getHistory")
+def getHistory():
+    emit("result", {
+        "type": "history-update",
+        "history": history["log"][:5]
+    })
+
+
+def updateOBSFiles():
+    with open("obs/timer.txt", "w+") as f:
+        seconds = timerTracker.getTimeLeft() % (24 * 3600)
+        hour = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+
+        f.seek(0)
+        f.write(f"{hour:02}:{minutes:02}:{seconds:02}")
+        f.truncate()
+
+    with open("obs/points.txt", "w+") as f:
+        f.seek(0)
+        f.write(f"{floor(history['points'])}")
+        f.truncate()
+
+    # print("UPDATE", seconds, history["points"])
+
+
+def threadUpdater():
+    print("[SYSTEM] THREAD MADE")
+    while True:
+        updateOBSFiles()
+        time.sleep(0.5)
+
+
 if __name__ == "__main__":
     extraDirs = [r'.\static', r'.\templates']
     extraFiles = extraDirs[:]
     for extraDir in extraDirs:
-        for dirname, dirs, files in walk(extraDir):
+        for dirname, dirs, files in os.walk(extraDir):
             for filename in files:
-                filename = path.join(dirname, filename)
-                if path.isfile(filename):
+                filename = os.path.join(dirname, filename)
+                if os.path.isfile(filename):
                     extraFiles.append(filename)
     print(extraFiles)
 
+    Path("obs").mkdir(parents=True, exist_ok=True)
+
+    thread = threading.Thread(target=threadUpdater, daemon=True)
+    thread.start()
+
     # app.run(port=5050, extra_files=extraFiles)
-    socketio.run(app=app, port=5050, debug=True, use_reloader=True, allow_unsafe_werkzeug=True, extra_files=extraFiles)
+    socketio.run(app=app, port=5050, debug=False, use_reloader=False, allow_unsafe_werkzeug=True, extra_files=extraFiles)
--- static/controller.js
+++ static/controller.js
@@ -6,6 +6,10 @@ socket.on('connect', function () {
 const subscriptionTypeHolderEl = document.getElementById("subscription-type-holder");
 const subscriptionTypeEl = document.getElementById("subscription-type");
 const subscriptionQuantityLabelEl = document.getElementById("subscription-quantity-label")
+const subscriptionQuantityEl = document.getElementById("subscription-quantity")
+const applyMessageEl = document.getElementById("apply-message");
+const historyEl = document.getElementById("history-table");
+const historyRowTemplateEl = document.getElementById("history-row-template");
 
 let settingsChanged = false;
 
@@ -27,7 +31,11 @@ function UpdateApplyButtonStatus() {
 
 function ApplyButtonPressed() {
     if (settingsChanged) {
-        socket.emit("addSeconds", {seconds: 10});
+        // socket.emit("addToTimer", {type: subscriptionTypeEl.textContent, quantity: document.getElementById("subscription-quantity").value});
+        socket.emit("addTimerInfo", {
+            type: subscriptionTypeEl.getAttribute("data-type"),
+            quantity: subscriptionQuantityEl.value
+        });
     }
 
     settingsChanged = false;
@@ -35,22 +43,51 @@ function ApplyButtonPressed() {
 }
 
 
-
 document.getElementById("subscription-quantity").addEventListener("input", () => {
     settingsChanged = true;
     UpdateApplyButtonStatus();
 });
 
+socket.on('result', function (data) {
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
 
-console.log(subscriptionTypeHolderEl)
+    if (data["type"] === "add-error") {
+        applyMessageEl.textContent = `[FAILED] Last Applied at ${today.toUTCString()} due to "${data['reason']}"`
+    }
+    else if (data["type"] === "add-info") {
+        applyMessageEl.textContent = `[SUCCESSFUL] Last Applied at ${today.toUTCString()}}"`
+    }
+    else if (data["type"] === "history-update") {
+        historyEl.innerHTML = "";
+        for (const info of data["history"]) {
+            let rowEl = historyRowTemplateEl.innerHTML;
+            rowEl = rowEl.replaceAll("..1", info["#"])
+            rowEl = rowEl.replaceAll("..2", info["type"])
+            rowEl = rowEl.replaceAll("..3", info["quantity"])
+            rowEl = rowEl.replaceAll("..4", info["seconds"])
+            rowEl = rowEl.replaceAll("..5", info["points"])
+
+            historyEl.insertAdjacentHTML("beforeend", rowEl);
+        }
+    }
+});
+
+
 for (const child of subscriptionTypeHolderEl.children) {
     child.addEventListener("click", function () {
         subscriptionTypeEl.textContent = child.textContent;
+        subscriptionTypeEl.setAttribute("data-unit", child.getAttribute("data-unit"));
+        subscriptionTypeEl.setAttribute("data-type", child.getAttribute("data-type"));
 
         subscriptionQuantityLabelEl.textContent = "QUANTITY"
         const unit = child.getAttribute("data-unit")
         if (unit !== "") {
             subscriptionQuantityLabelEl.textContent += ` [${unit}]`
+
+            settingsChanged = true;
+            UpdateApplyButtonStatus();
         }
     });
 }
+socket.emit("getHistory");
